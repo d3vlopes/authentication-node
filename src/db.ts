650783104bc61b37e4cb320b22ffa8d1e4953701
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-10-12 01:50:14 -0300

    config db

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,6 @@
+require('dotenv/config')
 import { Pool } from 'pg'
 
-const connectionString = ""
+const connectionString = process.env.DB_CONNECTION
 
 export const db = new Pool( { connectionString })
