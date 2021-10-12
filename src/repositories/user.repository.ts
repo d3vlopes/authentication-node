293007267093367ddd321d418669a899eb0d0ed9
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-10-12 20:03:13 -0300

    add method for remove user

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -40,9 +40,30 @@ class UserRepository {
 
     const values = [user.username, user.password];
     const { rows } = await db.query<{ uuid: string }>(query, values);
-    const [newUser] = rows
-    
-    return newUser.uuid
+    const [newUser] = rows;
+
+    return newUser.uuid;
+  }
+
+  async update(user: User) {
+    const query = `
+      UPDATE application_user 
+      SET username = $1, password = crypt($2, 'my_salt')
+      WHERE uuid = $3
+    `;
+
+    const values = [user.username, user.password, user.uuid];
+    await db.query(query, values);
+  }
+
+  async remove(uuid: string) {
+    const query = `
+      DELETE FROM application_user
+      WHERE uuid = $1 
+    `
+
+    const values = [uuid]
+    await db.query<{ uuid: string }>(query, values)
   }
 }
 
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -25,15 +25,20 @@ usersRoute.post('/users', async (req: Request, res: Response, next: NextFunction
   res.status(statusCodes.CREATED).send(uuid)
 })
 
-usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+usersRoute.put('/users/:uuid', async (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
   const uuid = req.params.uuid
   const modifiedUser = req.body
 
   modifiedUser.uuid = uuid
 
-  res.status(statusCodes.OK).send(modifiedUser)
+  await userRepository.update(modifiedUser)
+
+  res.status(statusCodes.OK).send()
 })
 
-usersRoute.delete('/users/:uuid', (req: Request, res: Response, next: NextFunction) => {
+usersRoute.delete('/users/:uuid', async (req: Request, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid
+  
+  await userRepository.remove(uuid)
   res.sendStatus(statusCodes.OK)
 })
